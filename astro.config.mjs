e940199caf2b159cb74e4e73ced7403be65245e5
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-09-06 21:16:37 +0530

    fix: remove react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.thesobercoder.in",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
 });
