cf9ed4346f274473b9db3332af52cdde4e0bdfea
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-09-08 21:30:25 +0530

    feat: add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.thesobercoder.in",
   integrations: [tailwind()],
+  adapter: vercel({
+    analytics: true,
+  }),
 });
