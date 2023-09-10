402f6c3e6e195302f732645a39d4412676ae2ca6
Soham Dasgupta <soham1.dasgupta@gmail.com>
2023-09-10 19:48:49 +0530

    feat: add sitemap.xml and robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.thesobercoder.in",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   adapter: vercel({
     analytics: true,
   }),
