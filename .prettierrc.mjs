a679586d37ed2cc619b786935734882b6aa86077
Soham Dasgupta <soham1.dasgupta@gmail.com>
2024-01-17 15:54:12 +0530

    fix: prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,8 +1,8 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: [
-    "prettier-plugin-tailwindcss",
     "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
     "prettier-plugin-organize-imports",
   ],
   astroAllowShorthand: false,
