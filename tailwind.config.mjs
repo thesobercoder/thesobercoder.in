6bd0d608e5ea35b78e5faffd75ff0c6e22a23530
Soham Dasgupta <soham1.dasgupta@gmail.com>
2024-01-27 12:44:32 +0530

    feat: improve theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,10 +17,17 @@ module.exports = {
       {
         "catppuccin-latte": {
           primary: "#1e66f5", // blue
-          secondary: "#ea76cb", // pink
+          "primary-content": "#eff1f5", // base
+          secondary: "#8839ef", // mauve
+          "secondary-content": "#eff1f5", // base
           accent: "#179299", // teal
-          neutral: "#dce0e8", // crust
+          "accent-content": "#eff1f5", // base
+          neutral: "#ccd0da", // surface0
+          "neutral-content": "#4c4f69", // text
           "base-100": "#eff1f5", // base
+          "base-200": "#e6e9ef", // mantle
+          "base-300": "#e6e9ef", // crust
+          "base-content": "#4c4f69", // text
           info: "#209fb5", // sapphire
           success: "#40a02b", // green
           warning: "#df8e1d", // yellow
@@ -28,10 +35,17 @@ module.exports = {
         },
         "catppuccin-frappe": {
           primary: "#8caaee", // blue
-          secondary: "#f4b8e4", // pink
+          "primary-content": "#303446", // base
+          secondary: "#ca9ee6", // mauve
+          "secondary-content": "#303446", // base
           accent: "#81c8be", // teal
-          neutral: "#232634", // crust
+          "accent-content": "#303446", // base
+          neutral: "#414559", // surface0
+          "neutral-content": "#c6d0f5", // text
           "base-100": "#303446", // base
+          "base-200": "#292c3c", // mantle
+          "base-300": "#232634", // crust
+          "base-content": "#c6d0f5", // text
           info: "#85c1dc", // sapphire
           success: "#a6d189", // green
           warning: "#e5c890", // yellow
