541f38011055972e813a6e8cee8343a660ebc871
Soham Dasgupta <soham@thesobercoder.in>
2024-10-19 01:39:41 +0530

    feat: add portfolio landing page layout

---
+++ src/app/(portfolio)/page.tsx
@@ -0,0 +1,130 @@
+import ShinyLink from "@/components/ui/shiny-link";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <div className="relative hero z-10">
+        <div className="mx-auto min-h-screen max-w-screen-2xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+          <div className="lg:flex lg:justify-between lg:gap-4">
+            <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+              <div className="flex lg:items-start gap-4 flex-col lg:flex-row items-center text-center lg:text-left">
+                <Image
+                  src="/avatar.webp"
+                  alt="Soham Dasgupta"
+                  width={64}
+                  height={64}
+                  className="rounded-full"
+                />
+                <div>
+                  <Link
+                    href="/"
+                    className="text-4xl lg:text-5xl block font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 pb-1"
+                  >
+                    <h1>Soham Dasgupta</h1>
+                  </Link>
+                  <h2 className="text-sm lg:text-lg font-medium text-neutral-400">
+                    Technology Leader & Enterprise Architect
+                  </h2>
+                  <div className="flex flex-row justify-center lg:justify-start gap-x-2 w-full mt-4">
+                    <ShinyLink
+                      rel="noreferrer noopener"
+                      target="_self"
+                      href="mailto:soham@thesobercoder.in"
+                      className="w-32 py-4 rounded-xl"
+                      aria-label="Email"
+                    >
+                      Email
+                    </ShinyLink>
+                    <ShinyLink
+                      rel="noreferrer noopener"
+                      target="_blank"
+                      href="/resume.pdf"
+                      className="w-32 py-4 rounded-xl"
+                      aria-label="Resume"
+                    >
+                      Resume
+                    </ShinyLink>
+                  </div>
+                  {/* {
+                SOCIALS.length && (
+                  <ul
+                    className="flex items-center justify-between lg:justify-start gap-x-7 mt-6 w-full"
+                    aria-label="Social media"
+                  >
+                    {SOCIALS.map((social) => (
+                      <li className="text-xs p-2 rounded" data-ccursor>
+                        <a
+                          className="block hover:text-slate-200"
+                          href={social.url}
+                          target={social.target}
+                          rel={social.rel}
+                          aria-label={`${social.name} (opens in a new tab)`}
+                        >
+                          <span className="sr-only">{social.name}</span>
+                          {<Component component={social.icon} />}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                )
+              } */}
+                </div>
+              </div>
+            </header>
+            <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
+              <section
+                id="about"
+                className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+                aria-label="About me"
+              >
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-3 backdrop-blur-lg md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                  <h2 className="text-sm font-bold uppercase tracking-widest text-neutral-200 lg:sr-only">
+                    About
+                  </h2>
+                </div>
+                <div>
+                  <div className="mb-4 space-y-4 text-lg">
+                    <p>
+                      I’m a technology leader with 15 years of experience in
+                      software engineering and enterprise architecture. I am
+                      passionate about collaboration and mentorship, guiding
+                      teams to deliver innovative solutions. I thrive on driving
+                      growth and efficiency in technology initiatives and am
+                      committed to fostering a culture of creativity and
+                      problem-solving within my teams.
+                    </p>
+                    <p>
+                      Looking forward to new challenges and exciting
+                      opportunities!
+                    </p>
+                  </div>
+                </div>
+              </section>
+              <section
+                id="experience"
+                className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+                aria-label="Work experience"
+              >
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-3 backdrop-blur-lg md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                  <h2 className="text-sm font-bold uppercase tracking-widest text-neutral-200 lg:sr-only">
+                    Experience
+                  </h2>
+                </div>
+                <div>
+                  {/* <ol className="group/list space-y-12">
+                {EXPERIENCE.map((experience) => <Experience {...experience} />)}
+              </ol> */}
+                </div>
+              </section>
+            </main>
+          </div>
+        </div>
+      </div>
+      <footer className="flex flex-col items-center w-full pb-4 text-xs">
+        <span> {new Date().getFullYear()} All rights reserved.</span>
+      </footer>
+    </>
+  );
+}
