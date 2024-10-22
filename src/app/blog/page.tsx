28f68c963d2c488b8a9bb2d9e26c0d25607106c7
Soham Dasgupta <soham@thesobercoder.in>
2024-10-22 22:17:35 +0530

    feat: add dynamic image loading with skeleton fallback

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,46 +1,50 @@
-import Image from "next/image";
+import dynamicImport from "next/dynamic";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
-export default async function Blog() {
-  type Articles = Array<{
-    type_of: string;
-    id: number;
-    title: string;
-    description: string;
-    readable_publish_date: string;
-    slug: string;
-    path: string;
-    url: string;
-    comments_count: number;
-    public_reactions_count: number;
-    collection_id: number | null;
-    published_timestamp: string;
-    positive_reactions_count: number;
-    cover_image: string;
-    social_image: string;
-    canonical_url: string;
-    created_at: string;
-    edited_at: string | null;
-    crossposted_at: string | null;
-    published_at: string;
-    last_comment_at: string;
-    reading_time_minutes: number;
-    tag_list: Array<string>;
-    tags: string;
-    user: {
-      name: string;
-      username: string;
-      twitter_username: string;
-      github_username: string;
-      user_id: number;
-      website_url: string;
-      profile_image: string;
-      profile_image_90: string;
-    };
-  }>;
+const DynamicImageWithSkeleton = dynamicImport(
+  () => import("@/components/image-with-skeleton")
+);
 
+type Articles = Array<{
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  readable_publish_date: string;
+  slug: string;
+  path: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  collection_id: number | null;
+  published_timestamp: string;
+  positive_reactions_count: number;
+  cover_image: string;
+  social_image: string;
+  canonical_url: string;
+  created_at: string;
+  edited_at: string | null;
+  crossposted_at: string | null;
+  published_at: string;
+  last_comment_at: string;
+  reading_time_minutes: number;
+  tag_list: Array<string>;
+  tags: string;
+  user: {
+    name: string;
+    username: string;
+    twitter_username: string;
+    github_username: string;
+    user_id: number;
+    website_url: string;
+    profile_image: string;
+    profile_image_90: string;
+  };
+}>;
+
+export default async function Blog() {
   const res = await fetch(process.env.DEV_TO_API_URL!, {
     headers: {
       "Content-Type": "application/json",
@@ -73,12 +77,11 @@ export default async function Blog() {
               href={article.url}
               className="block overflow-hidden rounded-lg border border-foreground/20"
             >
-              <Image
+              <DynamicImageWithSkeleton
                 src={article.social_image}
                 alt={article.title}
-                height={500}
-                width={1000}
-                className="w-full object-cover transition-transform transform group-hover:scale-105 filter brightness-75 group-hover:brightness-50"
+                height={190}
+                width={380}
               />
               <div className="absolute inset-0 flex items-center justify-center transition-opacity opacity-0 bg-black bg-opacity-25 group-hover:opacity-100 group-hover:bg-opacity-50">
                 <svg
