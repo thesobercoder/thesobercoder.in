713198af0163c52977ac5febacfee5c34784129c
Soham Dasgupta <soham@thesobercoder.in>
2025-02-16 23:43:30 +0530

    feat: add protection for production stage in SST configuration

--- sst.config.ts
+++ sst.config.ts
@@ -5,6 +5,7 @@ export default $config({
     return {
       name: "thesobercoder-stack",
       removal: input?.stage === "production" ? "retain" : "remove",
+      protect: input?.stage === "production",
       home: "aws",
     };
   },
