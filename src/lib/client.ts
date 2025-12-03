5948509bd0f1e6b3f9cdb2e34e483fb84a84a39c
Soham Dasgupta <soham@thesobercoder.in>
2025-12-03 23:38:45 +0530

    refactor: remove unused dependencies and components

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,13 +4,3 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
-
-export const formatDate = (date: Date): string => {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    timeZone: "UTC",
-  };
-  return new Intl.DateTimeFormat("en-US", options).format(date);
-};
